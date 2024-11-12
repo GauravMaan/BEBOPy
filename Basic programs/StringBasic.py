84c4d82083f618ed1778672664cb39584c90b530
gauravmaan <gauravmaan0009@gmail.com>
2024-11-12 12:27:59 +0530

    final commit

---
+++ Basic programs/StringBasic.py
@@ -0,0 +1,46 @@
+#string is immutable.
+
+# s="Gaurav"
+# s[0]="V"
+# print(s) #TypeError: 'str' object does not support item assignment
+
+# b='Gaurav'
+# print(id(b))
+
+# c=str("gaurav")
+# print(c)
+
+# d=str('gaurav')
+# print(d)
+
+# n=''
+# n=""
+# s=str()
+
+# a="gaurav "
+# b="maan"
+# c=a+b
+# print(c)
+# print(a*10)
+
+# str="welcome"
+# print(str[1:6])
+# print(str[:-6])
+# print(str[-4:-1])
+# print(str[:-2])
+#
+# print(ord("V"))
+# print(ord("g"))
+#
+# print(ord("A"))
+# print(chr(65))
+
+# print(max("Gv"))
+
+# print(len("Gaurav"))
+
+s="welcome"
+print("come" in s)
+print("come" not in s)
+print("cme" in s)
+print("cme" not in s)
