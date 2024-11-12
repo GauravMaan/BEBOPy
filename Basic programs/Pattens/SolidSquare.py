3c22850538e9e7878d1707fd97d86c34deca20e0
gauravmaan <gauravmaan0009@gmail.com>
2024-11-12 17:31:00 +0530

    final commit

--- Basic programs/Pattens/SolidSquare.py
+++ Basic programs/Pattens/SolidSquare.py
@@ -6,10 +6,17 @@
 #         print("#",end=" ")
 #     print()
 
-n=5
-count=0
+# n=5
+# count=0
+# for i in range(n):
+#     for c in range(n):
+#         count+=1
+#         print(f"{count:2}",end=" ")
+#     print()
+
+n = int(input("Enter the number: "))
+
 for i in range(n):
-    for c in range(n):
-        count+=1
-        print(f"{count:2}",end=" ")
+    for j in range(n):
+        print((i + j) % 2, end=" ")
     print()
