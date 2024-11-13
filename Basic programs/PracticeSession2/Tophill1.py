6217aedca80cb43c0116d2ff0902281850c90cb1
gauravmaan <gauravmaan0009@gmail.com>
2024-11-13 11:03:12 +0530

    final commit

---
+++ Basic programs/PracticeSession2/Tophill1.py
@@ -0,0 +1,24 @@
+
+n = 4
+count = 1
+for i in range(n):
+    for s in range(1, n - i):
+        print("  ", end=" ")
+    for c in range(1, i + 1):
+        print(f"{count:2}", end=" ")
+        count += 1
+    for c in range(i+1):
+        print(f"{count:2}", end=" ")
+        count+=1
+    print()
+
+# n = int(input("Enter the number: "))
+# for i in range(1, n + 1):
+#     for s in range(1, n - i):
+#         print(" ", end=" ")
+#     for c in range(1, i):
+#         print(c, end=" ")
+#     for c in range(i, 0, -1):
+#         print(c, end=" ")
+#     print()
+
