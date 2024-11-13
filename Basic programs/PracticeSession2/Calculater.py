409153b84d9a66cdf6a8f7cd152427d0f75dc05c
gauravmaan <gauravmaan0009@gmail.com>
2024-11-13 10:58:06 +0530

    final commit

--- Basic programs/PracticeSession2/Calculater.py
+++ Basic programs/PracticeSession2/Calculater.py
@@ -3,9 +3,7 @@ print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
-
 choice = input("Enter choice (1/2/3/4): ")
-
 if choice in ('1', '2', '3', '4'):
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
