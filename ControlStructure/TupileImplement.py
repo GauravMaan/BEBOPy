c860319508b501eec258efd4ea20437eb1be8284
gauravmaan <gauravmaan0009@gmail.com>
2024-11-18 11:31:59 +0530

    final commit

--- ControlStructure/TupileImplement.py
+++ ControlStructure/TupileImplement.py
@@ -1,32 +1,56 @@
 # Defining a tuple
-tuple = (1, 2, 3, 4, 5)
+Tuple = (1, 2, 3, 4, 5)
 # Tuple with mixed data types
 mixed_tuple = (1, "hello", 3.14, True)
 #Accessing Tuple
-print(tuple[0])
+print(Tuple[0])
 print(mixed_tuple[1])
 print("<---------------------------------------->")
 
-print(tuple[1:4])
+
+print(Tuple[1:4])
 print("<---------------------------------------->")
+
+
 # Counting occurrences of a value
-print(tuple.count(3))
+print(Tuple.count(3))
 print("<---------------------------------------->")
+
+
 # Finding the index of a value
-print(tuple.index(4))
+print(Tuple.index(4))
 print("<---------------------------------------->")
-a, b, c, d, e = tuple
+
+
+a, b, c, d, e = Tuple
 print(a)
 print(b)
 print("<---------------------------------------->")
+
+
 #tuple with single element
 tuple1=(1,)
 print(type(tuple1))
 print("<---------------------------------------->")
+
+
 #iteration on tuple
 tuple1=(1,2,3,4,4)
 for i in tuple1:
     print(i)
+print("<---------------------------------------->")
+#tuple to list
+Tuple3=(1,2,3,4,5)
+list=list(Tuple3)
+list[1]="Gaurav"
+list.insert(0,9)
+ans=tuple(list)
+print(ans)
+
+print("<---------------------------------------->")
+tuple2=(1,2,3,2,1)
+del(tuple2)
+print(tuple2)
 print("<---------------------------------------->")
 
 tuple[0] = 10  # This will raise a TypeError
