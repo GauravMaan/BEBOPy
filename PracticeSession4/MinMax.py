fa7f9b5077843aeab24bd3459c2037c652b76f29
gauravmaan <gauravmaan0009@gmail.com>
2024-11-19 11:45:15 +0530

    final commit

--- PracticeSession4/MinMax.py
+++ PracticeSession4/MinMax.py
@@ -1,10 +1,9 @@
 n = tuple(map(int, input("Enter tuple: ").split()))
 mini = n[0]
-max = n[0]
+maxi = n[0]
 for i in n:
     if i < mini:
         mini = i
     else:
-        max = i
-result = (mini, max)
-print("Output:", result)
+        maxi = i
+print("Output:", (mini, maxi))
