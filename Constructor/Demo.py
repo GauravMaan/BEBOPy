54ee6825345e096b092dc672b4f746cddc193151
gauravmaan <gauravmaan0009@gmail.com>
2024-12-04 11:08:51 +0530

    final commit

--- Constructor/Demo.py
+++ Constructor/Demo.py
@@ -3,6 +3,16 @@
 #it can also receive the arguments and parameter
 #it is called automatically when obj is created
 
+
+# In Python, a constructor is a special method used to initialize the attributes of an object when the object is created. This method is named __init__() and is automatically called when an object of the class is instantiated.
+#
+# Key Points about Constructors:
+#
+# 	1.	Purpose: The constructor is used to assign initial values to the instance variables of a class.
+# 	2.	Special Name: The constructor method is always named __init__ and must have at least one parameter (self), which refers to the instance being created.
+# 	3.	Default Constructor: If a class doesn’t define a constructor, Python provides a default constructor that doesn’t perform any initialization.
+# 	4.	Parameterized Constructor: A constructor can take parameters to initialize attributes with specific values.
+
 # class demo:
 #     def __init__(self):
 #         print("hello")
