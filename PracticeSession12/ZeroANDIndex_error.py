d75763e902705f0141a7c69e1ff656637083572a
gauravmaan <gauravmaan0009@gmail.com>
2024-12-11 13:54:21 +0530

    final commit

--- PracticeSession12/ZeroANDIndex_error.py
+++ PracticeSession12/ZeroANDIndex_error.py
@@ -1,11 +1,12 @@
 try:
-    a, b = map(int, input("Enter the value: ").split())
-    result = a / b
-    print(result)
-    lst = [10, 20, 30]
-    idx = int(input("Enter the index: "))
-    print(lst[idx])
-except ZeroDivisionError:
-    print("Division by zero is not allowed.")
+    try:
+        a, b = map(int, input("Enter the value: ").split())
+        result = a / b
+        print(result)
+    except ZeroDivisionError:
+        print("Division by zero is not allowed.")
+        lst = [10, 20, 30]
+        idx = int(input("Enter the index: "))
+        print(lst[idx])
 except IndexError:
     print("Index out of range.")
