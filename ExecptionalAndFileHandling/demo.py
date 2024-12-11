6d0ce7f1194bd4d42e8c4f63f7fbd2d181403b7f
gauravmaan <gauravmaan0009@gmail.com>
2024-12-11 12:45:41 +0530

    final commit

--- ExecptionalAndFileHandling/demo.py
+++ ExecptionalAndFileHandling/demo.py
@@ -1,22 +1,35 @@
 
-print("Yooo")
-try:
-    print(10/0)
-    print(10/5)
-    print(10/0)
+# print("Yooo")
+# try:
+#     print(10/0)
+#     print(10/5)
+#     print(10/0)
 # except:
 #    print("wrong code")
-except Exception as a:
-    print(a)
+# except Exception as a:
+#     print(a)
+#
+# n=10
+# n1=0
+# try:
+#     ans=n-n1
+#     print(ans)
+# except Exception as a:
+#     print(a)
+# else:
+#     print("koi exception nhi hai")
+# finally:
+#     print("chal gya code")
 
-n=10
-n1=0
-try:
-    ans=n-n1
-    print(ans)
-except Exception as a:
-    print(a)
-else:
-    print("koi exception nhi hai")
-finally:
-    print("chal gya code")
+# def yoo():
+#     raise ValueError("An error occurred.")
+# try:
+#     age = int(input("Enter age: "))
+#     if age % 2 == 0:
+#         print("even")
+#     else:
+#         print("odd")
+# except ValueError:
+#     print("Invalid input. Please enter a valid integer.")
+#     yoo()
+
