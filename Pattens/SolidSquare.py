00c2df58d4777956fbce64d48a398d1f235033bd
gauravmaan <gauravmaan0009@gmail.com>
2024-12-12 21:43:18 +0530

    final commit

--- Pattens/SolidSquare.py
+++ Pattens/SolidSquare.py
@@ -6,11 +6,13 @@
 #         print("#",end=" ")
 #     print()
 
+# n=5
+# count=0
+# for i in range(n):
+#     for c in range(n):
+#         count+=1
+#         print(f"{count:2}",end=" ")
+#     print()
 n=5
-count=0
 for i in range(n):
-    for c in range(n):
-        count+=1
-        print(f"{count:2}",end=" ")
-    print()
-
+    print("* " * n)
