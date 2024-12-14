7e5458f8a741dc84ce776764b7996829dbf582de
gauravmaan <gauravmaan0009@gmail.com>
2024-12-14 14:26:18 +0530

    final commit

--- String/StringRev.py
+++ String/StringRev.py
@@ -1,9 +1,20 @@
-# s="welcome"
-# s2=" "
-# for i in s:
-#     s2=i+s2
-# print(s2)
+s="welcome bro"
+s2=" "
+for i in s:
+    s2=i+s2
+print(s2)
 
-s="vivek"
+s="vivek noob"
 s2=s[::-1]
 print(s2)
+
+s1="gaurav pro"
+a=s1.split()
+empty=[]
+for i in a:
+    s2=""
+    for j in i:
+        s2=j+s2
+    empty.append(s2)
+result=" ".join(empty)
+print(result)
