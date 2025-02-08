e05e4bed1341f87565f596eec2f0341530d85103
GauravMaan <gauravmaan0009@gmail.com>
2025-02-08 15:12:48 +0530

    final commit

--- Collection/FindMissingValue.py
+++ Collection/FindMissingValue.py
@@ -5,3 +5,17 @@ def find_missing_number(nums):
     return expected_sum - actual_sum
 nums = [1, 2, 4, 5, 6]
 print(find_missing_number(nums))
+
+
+def find_missing_number(nums):
+    n = len(nums) + 1
+    expected_sum = n * (n + 1) // 2
+
+    actual_sum = 0
+    for num in nums:
+        actual_sum += num
+
+    return expected_sum - actual_sum
+
+nums = [1, 2, 4, 5, 6]
+print(find_missing_number(nums))
