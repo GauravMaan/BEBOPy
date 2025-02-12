7fcbbda55caf602527b0eb37d8fbee04238943ca
GauravMaan <gauravmaan0009@gmail.com>
2025-02-12 17:46:35 +0530

    final commit

--- Basic programs/Factorial.py
+++ Basic programs/Factorial.py
@@ -8,9 +8,19 @@
 # print("Factorial of is", f)
 
 
-#using For loop
-N = int(input("Enter the number: "))
-f = 1
-for i in range(1, N + 1):
-    f *= i
-print("Factorial is", f)
+# #using For loop
+# N = int(input("Enter the number: "))
+# f = 1
+# for i in range(1, N + 1):
+#     f *= i
+# print("Factorial is", f)
+
+
+
+def fact(n):
+    f = 1
+    for i in range(1,n+1):
+        f*=i
+    return f
+n=5
+print(fact(n))
