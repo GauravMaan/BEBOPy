dd6be280ba98386f87a3e7db3064abaade9317c3
GauravMaan <gauravmaan0009@gmail.com>
2025-02-12 17:42:21 +0530

    final commit

--- Collection/List_sum.py
+++ Collection/List_sum.py
@@ -1,13 +1,28 @@
-def pairs(lst, target):
-    pairs = []
-    demo = set()
-    for i in lst:
-        j = target - i
-        if j in demo:
-            pairs.append((j, i))
-        demo.add(i)
+# def pairs(lst, target):
+#     pairs = []
+#     demo = set()
+#     for i in lst:
+#         j = target - i
+#         if j in demo:
+#             pairs.append((j, i))
+#         demo.add(i)
+#
+#     return pairs
+#
+#
+# print(pairs([1, 2, 3, 4, 5, 6], 7))
 
-    return pairs
 
 
-print(pairs([1, 2, 3, 4, 5, 6], 7))
+def find_pairs(lst,key):
+    n=len(lst)
+    ans=[]
+    for i in range (n):
+        for j in range(i+1,n):
+            if lst[i]+lst[j]==key:
+                temp=lst[i],lst[j]
+                ans.append(temp)
+    print(ans)
+lst=[1,2,3,4,5,6]
+key=7
+find_pairs(lst,key)
